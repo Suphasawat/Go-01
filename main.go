4259fbf2f8a9055c6249cf34d13a4c9e62f7da61
Nine <rbxninez@gmail.com>
2025-04-09 18:07:50 +0700

    Implement code changes to enhance functionality and improve performance

--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"strconv"
-
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -14,7 +12,7 @@ type Book struct {
 
 var books = []Book{}
 
-func main() {
+ func main() {
   app := fiber.New()
   
   books = append(books, Book{ID: 1, Title: "Nine", Author: "NineLnwZa007"})
@@ -23,31 +21,9 @@ func main() {
   app.Get("/books", getBooks)
   app.Get("/books/:id", getBook)
   app.Post("/books", createBook)
+  app.Put("/books/:id", updateBook)
+  app.Delete("/books/:id", deleteBook)
 
   app.Listen((":8080"))
 }
 
-func getBooks(c *fiber.Ctx) error  {
-  return c.JSON(books)  
-}
-
-func getBook(c *fiber.Ctx) error {
-  bookId, err := strconv.Atoi(c.Params("id"))
-
-  if err != nil { 
-    return c.Status(fiber.StatusBadRequest).SendString(err.Error())
-  }
-
-  for _, book := range books {
-    if book.ID == bookId {
-      return c.JSON(book)
-    }
-  }
-  return c.SendStatus(fiber.StatusNotFound)
-}
-
-func createBook(c *fiber.Ctx) error {
-  book := new(Book)
-  c.BodyParser(book)
-  return c.JSON(book)
-}
